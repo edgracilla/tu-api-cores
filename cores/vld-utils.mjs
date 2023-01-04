584c8aa47d8333147344782684d1daf31f9e9913
Edd Gracilla <ed.gracilla@gmail.com>
2023-01-04 21:06:22 +0800

    export vld-utils

---
+++ cores/vld-utils.mjs
@@ -0,0 +1,121 @@
+/* eslint-disable no-await-in-loop */
+
+import Ajv from 'ajv';
+import _get from 'lodash.get';
+import mongoose from 'mongoose';
+import MongoQS from 'mongo-querystring';
+import ApiError from './api-error.mjs';
+
+const ajv = new Ajv({
+  keywords: ['prereq'],
+  allErrors: true,
+});
+
+export const makeMongoQS = (params) => {
+  const whiteParams = params
+    .filter(Boolean)
+    .reduce((accum, key) => {
+      accum[key] = true;
+      return accum;
+    }, {});
+
+  return new MongoQS({
+    whitelist: whiteParams,
+    blacklist: {
+      page: true,
+      sort: true,
+      limit: true,
+      near: true,
+      search: true,
+    },
+  });
+};
+
+const stripperSlave = (obj, ret) => {
+  const keys = Object.keys(obj);
+
+  for (let i = 0; i < keys.length; i += 1) {
+    const key = keys[i];
+    const val = obj[key];
+
+    if (key !== 'required') {
+      if (typeof val === 'object' && !Array.isArray(val)) {
+        ret[key] = {};
+        stripperSlave(val, ret[key]);
+      } else {
+        ret[key] = val;
+      }
+    }
+  }
+};
+
+export const stripSchemaRequired = (schema) => {
+  const newObj = {};
+  stripperSlave(schema, newObj);
+  return newObj;
+};
+
+export function makeValidationSchemas(body, query) {
+  return {
+    body,
+    postSchema: { schema: { body } },
+    getSchema: { schema: { querystring: query } },
+    patchSchema: { schema: { body: stripSchemaRequired(body) } },
+  };
+}
+
+export function getPrereqs(body, version) {
+  const props = body.properties;
+  const fields = Object.keys(props || {});
+
+  const ret = fields.map((field) => {
+    const model = _get(props, `${field}.prereq`);
+    return model ? [field, `${version}-${model}`] : null;
+  });
+
+  return ret.filter(Boolean);
+}
+
+export async function prereqExists(body, prereqs) {
+  const errBag = [];
+
+  for (let i = 0; i < prereqs.length; i += 1) {
+    const field = prereqs[i][0];
+    const model = prereqs[i][1];
+    const _id = body[field];
+
+    if (_id) {
+      const exist = await mongoose
+        .model(model)
+        .countDocuments({ _id }, { limit: 1 });
+
+      if (!exist) {
+        errBag.push(`${field} '${_id}' does not exist.`);
+      }
+    }
+  }
+
+  if (errBag.length) {
+    throw new ApiError(400, 'Missing prerequisite record.', errBag);
+  }
+}
+
+export function CustomAjvError(from, ajvErrs) {
+  const errMsgs = ajvErrs.map((err) => `${from} ${err.message}`);
+  const err = new Error(errMsgs.join(', '));
+  err.statusCode = 400;
+
+  return err;
+}
+
+export function makeServiceCallValidator(from, schema) {
+  const ajvCompiled = ajv.compile(schema);
+
+  return async (data) => {
+    if (!ajvCompiled(data)) {
+      throw CustomAjvError(from, ajvCompiled.errors);
+    }
+  };
+}
+
+export default {};
--- index.mjs
+++ index.mjs
@@ -1,3 +1,5 @@
 export { default as ApiError } from './cores/api-error.mjs';
 export { logger, AuditLogger } from './cores/sys-logger.mjs';
 export { errHandler, apiResponse } from './cores/api-utils.mjs';
+
+export * as ValidationUtils from './cores/vld-utils.mjs';
