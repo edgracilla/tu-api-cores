04225f1766d6d74ee4430784e1d30773f355c09c
Edd Gracilla <ed.gracilla@gmail.com>
2023-01-04 20:43:08 +0800

    upd

--- index.mjs
+++ index.mjs
@@ -1,11 +1,9 @@
-import ApiError from './cores/api-error.mjs';
-
 import { logger, AuditLogger } from './cores/sys-logger.mjs';
 
+export { default as ApiError } from './cores/api-error.mjs';
 export { logger, AuditLogger };
 
 export default {
   logger,
-  ApiError,
   AuditLogger,
 };
